b9e0dd6dfc775ced1a3e5657e2bf73ebac2c5036
sumannandi121998 <57460338+sumannandi121998@users.noreply.github.com>
2019-11-07 03:21:43 -0800

    finding the power of 2 from a list

---
+++ power.py
@@ -0,0 +1,19 @@
+"""
+name: Suman Nandi
+Date: 06.11.2019"""
+
+from math import *
+L = [1,2,4,8,16,32,64]
+X = 5
+found = False
+i = 0
+while not found and i< len(L):
+    if 2**X == L[i]:
+        found = True
+    else:
+        i=i+1
+        
+if found :
+    print ("at index",i)
+else:
+    print (X,"not found")
